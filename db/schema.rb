7b49d8b0fd062aff07f2ff1553f3865d9dd2aa6b
Walmir Neto <wfsneto@gmail.com>
2018-07-26 18:44:27 -0300

    Create model author

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_210433) do
+ActiveRecord::Schema.define(version: 2018_07_26_213029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "authors", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
